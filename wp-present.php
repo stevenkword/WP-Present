6e80108f0fa13ed09a0787a9c3238ec2de0589a2
Steven K Word <stevenword@gmail.com>
2013-10-30 06:04:57 -0400

    Bump scripts version number

--- wp-present.php
+++ wp-present.php
@@ -50,7 +50,7 @@ class WP_Present {
 	public $capability = 'edit_others_posts';
 	public $nonce_field = 'wp-present-nonce';
 	public $nonce_fail_message = "fail!";
-	public $scripts_version = 200131007;
+	public $scripts_version = 20131030;
 	public $default_theme = 'simple.css'; //moon, night, simple, serif, solarized
 	//public $max_num_slides = 250; //not currently used, proposed variable
 
