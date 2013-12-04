a27a449587f31d96f1ba99476ec10796d219aa75
Steven K Word <stevenword@gmail.com>
2013-12-04 02:31:44 -0500

    Populates Admin class and updates formatting in the Core class

--- includes/class-wp-present-core.php
+++ includes/class-wp-present-core.php
@@ -73,13 +73,6 @@ class WP_Present_Core {
 		// Template
 		add_filter( 'template_include', array( $this, 'filter_template_include' ) );
 
-		// Admin
-		add_action( 'admin_menu', array( $this, 'action_admin_menu' ) );
-		add_action( 'admin_enqueue_scripts', array( $this, 'action_admin_enqueue_scripts' ) );
-		add_action( 'admin_head', array( $this, 'action_admin_head' ), 20 );
-		add_action( 'save_post', array( $this, 'action_save_post' ) );
-		add_action( 'admin_footer', array( $this, 'action_admin_footer' ), 20 );
-
 		// Hide screen options
 		add_filter('screen_options_show_screen', '__return_false'); // a test
 
@@ -391,187 +384,12 @@ class WP_Present_Core {
 		<?php
 	}
 
-	/**
-	 * Add the necessary menu pages
-	 *
-	 * @return null
-	 */
-	public function action_admin_menu(){
-		global $menu, $submenu;
-
-		// Taxonomy Menu
-		$taxonomy_url = 'edit-tags.php?taxonomy=' . self::TAXONOMY_SLUG . '&post_type='.self::POST_TYPE_SLUG;
-		$post_type_url = 'edit.php?post_type=' . self::POST_TYPE_SLUG;
-
-
-		// Add the options page
-		add_submenu_page( $post_type_url, self::OPTION_TITLE, 'Options', self::CAPABILITY, self::OPTION_NAME, array( $this, 'options_page' ) );
-
-		// Rename the menu item
-		foreach( $menu as $menu_key => $menu_item ) {
-			if( self::POST_TYPE_NAME == $menu_item[0] ) {
-				$menu[ $menu_key ][0] = self::TAXONOMY_NAME;
-			}
-		}
-
-		// Move the taxonomy menu to the top
-		// TODO: It owuld be better to search for the keys based on url
-		foreach( $submenu as $submenu_key => $submenu_item ) {
-			if( isset( $submenu_item[15][0] ) && self::TAXONOMY_NAME == $submenu_item[15][0] ) {
-				// This is a bit of hackery.  I should search for these keys
-				$submenu[$submenu_key][2] = $submenu[$submenu_key][15];
-				unset( $submenu[$submenu_key][15] );
-
-				// Not a fan of the add new bit
-				unset( $submenu[$submenu_key][10] );
-				ksort( $submenu[$post_type_url] );
-			}
-		}
-	}
-
-	/**
-	 * Markup for the Options page
-	 *
-	 * @return null
-	 */
-	public function options_page(){
-		?>
-		<div id="wpbody">
-			<div id="wpbody-content" aria-label="Main content" tabindex="0">
-				<div class="wrap">
-					<h2><?php echo self::OPTION_TITLE; ?></h2>
-					<h3>Select a Theme</h3>
-						<p>Current Theme: <?php echo self::DEFAULT_THEME; ?></p>
-					<h3>Resolution</h3>
-						<p>1024x768</p>
-					<h3>Branding</h3>
-						<p><textarea>Branding HTML textarea goes here</textarea></p>
-					<h3>Coming soon</h3>
-						<?php
-						$master_plan_file = fopen( 'master.plan', 'r', __FILE__ );
-						while ( ! feof( $master_plan_file ) )
-							echo fgets( $master_plan_file ) . '<br />';
-						fclose( $master_plan_file );
-						?>
-				</div>
-				<div class="clear"></div>
-			</div><!-- wpbody-content -->
-			<div class="clear"></div>
-		</div>
-		<?php
-	}
-
-	/**
-	 * Enqueue necessary admin scripts
-	 *
-	 * @uses wp_enqueue_script
-	 * @return null
-	 */
-	public function action_admin_enqueue_scripts() {
-
-		// Admin Styles
-		wp_enqueue_style( 'wp-present-admin', $this->plugins_url . '/css/admin.css', '', self::SCRIPTS_VERSION );
-
-		// Admin Scripts
-		wp_enqueue_script( 'jquery-ui-sortable' );
-		wp_enqueue_script( 'jquery-ui-resizable' );
-		wp_enqueue_script( 'jquery-ui-dialog' );
-
-		wp_enqueue_script( 'wp-present-admin', $this->plugins_url . '/js/admin.js', array( 'jquery' ), self::SCRIPTS_VERSION, true );
-
-		//wp_enqueue_media();
-		wp_enqueue_style( 'media-views' );
-
-		if( isset( $_REQUEST[ 'tag_ID' ] ) )
-			wp_localize_script( 'wp-present-admin', 'presentation', $_REQUEST[ 'tag_ID' ] );
-	}
-
-	/**
-	 * Output for the admin <head>
-	 *
-	 * @return null
-	 */
-	public function action_admin_head() {
-
-		// Presentation dashicon
-	    echo '<style type="text/css">.mp6 #adminmenu #menu-posts-slide div.wp-menu-image:before { content: "\f181" !important; }</style>';
-
-		// Only add this variable on the edit taxonomy page
-		global $pagenow;
-		if( 'edit-tags.php' != $pagenow || ! isset( $_GET['taxonomy'] ) || self::TAXONOMY_SLUG != $_GET['taxonomy'] || ! isset( $_GET[ 'tag_ID' ] ) )
-			return;
-
-		$num_slides = ( isset( $_GET[ 'tag_ID' ] ) ) ? count( $this->get_associated_slide_ids( $_GET[ 'tag_ID' ], $_GET[ 'taxonomy' ] ) ) : '';
-
-		$slides_query = new WP_Query( array(
-			'post_type'     => self::POST_TYPE_SLUG, //post type, I used 'product'
-			'post_status'   => 'publish', // just tried to find all published post
-			'posts_per_page' => -1,  //show all
-			'tax_query' => array( array(
-				'taxonomy' 	=> self::TAXONOMY_SLUG,
-				'terms'		=> $_GET[ 'tag_ID' ]
-			) )
-		) );
-		$num_slides = (int) $slides_query->post_count;
-		unset( $slides_query );
-
-		wp_localize_script( 'wp-present-admin', 'WPPNumSlides', array( intval( $num_slides ) ) );
-
-		if( isset( $_REQUEST[ 'tag_ID' ] ) )
-			wp_localize_script( 'wp-present-admin', 'WPPTaxonomyURL', array( get_term_link( (int) $_GET[ 'tag_ID' ], self::TAXONOMY_SLUG ) ) );
-
-		// Make the admin outer-container div big enough to prevent wrapping
-		$column_width = 210;
-		$container_size = ( $num_slides + 1 ) * $column_width;
-		?>
-		<style type="text/css">
-			#container{ width: <?php echo $container_size; ?>px;}
-		</style>
-		<?php
-		unset( $num_slides );
-	}
-
-	/*
-	 * Save chosen primary presentaiton as post meta
-	 * @param int $post_id
-	 * @uses wp_verify_nonce, current_user_can, update_post_meta, delete_post_meta, wp_die
-	 * @action save_post
-	 * @return null
-	 */
-	public function action_save_post( $post_id ) {
-		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
-			return;
-
-		// Broken
-		//if ( ! isset( $_POST[ self::NONCE_FIELD ] ) || ! wp_verify_nonce( $_POST[ self::NONCE_FIELD ], self::NONCE_FIELD ) )
-			//return;
-
-		if ( 'page' == get_post_type( $post_id ) && ! current_user_can( 'edit_page', $post_id ) )
-				return;
-		elseif ( ! current_user_can( 'edit_post', $post_id ) )
-				return;
-
-		//wp_die( 'You must choose a presentation', 'ERROR', array( 'back_link' => true ) );
-	}
-
-	/**
-	 * Output for the admin <footer>
-	 *
-	 * @return null
-	 */
-	public function action_admin_footer() {
-		// Only run on the edit taxonomy page
-		global $pagenow;
-		if( 'edit-tags.php' != $pagenow || ! isset( $_GET['taxonomy'] ) || self::TAXONOMY_SLUG != $_GET['taxonomy'] )
-			return;
-	}
-
 	/* Find which slides are already found in the DB before auto-populating the backfill
 	 *
 	 * @return array
 	 */
 	public function get_associated_slide_ids( $term, $taxonomy ) {
-		$term_description =  $this->get_term_description( $term, $taxonomy );
+		$term_description =  self::get_term_description( $term, $taxonomy );
 
 		if( ! is_array( $term_description ) )
 			return false;
@@ -1030,24 +848,24 @@ class WP_Present_Core {
 
 		global $post, $wpdb;
 
-		$post_id = $_REQUEST[ 'id' ];
+		$post_id      = $_REQUEST[ 'id' ];
 		$safe_content = wp_kses_post( $_REQUEST[ 'content' ] );
-		$safe_title = sanitize_text_field( $_REQUEST[ 'title' ] );
+		$safe_title   = sanitize_text_field( $_REQUEST[ 'title' ] );
 		$thumbnail_id = esc_url( url_to_postid( $_REQUEST[ 'background-image' ] ) );
 
 		$safe_background_color = sanitize_text_field( $_REQUEST[ 'background-color' ] );
-		$safe_text_color = sanitize_text_field( $_REQUEST[ 'text-color' ] );
-		$safe_link_color = sanitize_text_field( $_REQUEST[ 'link-color' ] );
+		$safe_text_color       = sanitize_text_field( $_REQUEST[ 'text-color' ] );
+		$safe_link_color       = sanitize_text_field( $_REQUEST[ 'link-color' ] );
 
 		// Work around for getting the attachment id
-		$prefix = $wpdb->prefix;
-		$attachment = $wpdb->get_col($wpdb->prepare( "SELECT ID FROM " . $prefix . "posts" . " WHERE guid='%s';", esc_url( $_REQUEST[ 'background-image' ] ) ) );
+		$prefix       = $wpdb->prefix;
+		$attachment   = $wpdb->get_col($wpdb->prepare( "SELECT ID FROM " . $prefix . "posts" . " WHERE guid='%s';", esc_url( $_REQUEST[ 'background-image' ] ) ) );
 		$thumbnail_id = ( isset( $attachment[0] ) ) ? $attachment[0] : false;
 
 		$updated_post = array(
-			'ID' => $post_id,
+			'ID'           => $post_id,
 			'post_content' => $safe_content,
-			'post_title' => $safe_title,
+			'post_title'   => $safe_title,
 		);
 		wp_update_post( $updated_post );
 
@@ -1083,15 +901,15 @@ class WP_Present_Core {
 
 		global $post;
 		$safe_content = wp_kses_post( $_REQUEST[ 'content' ] );
-		$safe_title = sanitize_text_field( $_REQUEST[ 'title' ] );
+		$safe_title   = sanitize_text_field( $_REQUEST[ 'title' ] );
 
 		$presentation = get_term_by( 'id', $_REQUEST['presentation'], self::TAXONOMY_SLUG );
 
 		$new_post = array(
-			'post_title' => ( $safe_title ) ? $safe_title : strip_tags( $safe_content ),
+			'post_title'   => ( $safe_title ) ? $safe_title : strip_tags( $safe_content ),
 			'post_content' => $safe_content,
-			'post_status' => 'publish',
-			'post_type' => self::POST_TYPE_SLUG
+			'post_status'  => 'publish',
+			'post_type'    => self::POST_TYPE_SLUG
 		);
 
 		$post_id = wp_insert_post( $new_post );
@@ -1132,7 +950,7 @@ class WP_Present_Core {
 			wp_die( $this->nonce_fail_message );
 		}
 
-		$presentation_id = $_REQUEST[ 'id' ];
+		$presentation_id  = $_REQUEST[ 'id' ];
 		$safe_description = sanitize_text_field( $_REQUEST[ 'content' ] );
 
 		$updated_presentation = array(
@@ -1165,9 +983,9 @@ class WP_Present_Core {
 	public function filter_image_size_names_choose( $sizes ) {
 		global $_wp_additional_image_sizes;
 		$sizes = array_merge( $sizes, array(
-			'reveal-small' => __( self::POST_TYPE_SINGULAR . ' Small' ),
+			'reveal-small'  => __( self::POST_TYPE_SINGULAR . ' Small' ),
 			'reveal-medium' => __( self::POST_TYPE_SINGULAR . ' Medium' ),
-			'reveal-large' => __( self::POST_TYPE_SINGULAR . ' Large' ),
+			'reveal-large'  => __( self::POST_TYPE_SINGULAR . ' Large' ),
 		) );
 		return $sizes;
 	}
