d192ec8c884e90e63ae8b1fbd641bfbf3170fd79
Steven K Word <stevenword@gmail.com>
2014-02-16 11:34:44 -0500

    Debug

--- inc/class-wp-present-core.php
+++ inc/class-wp-present-core.php
@@ -67,7 +67,7 @@ class WP_Present_Core {
 
 		// Setup
 		$this->plugins_url = plugins_url( '/wp-present' );
-		$this->nonce_fail_message = __( 'Cheatin&#8217; huh?' );
+		$this->nonce_fail_message = __( 'Core Cheatin&#8217; huh?' );
 
 		// Initialize
 		add_action( 'init', array( $this, 'action_init_register_post_type' ) );
