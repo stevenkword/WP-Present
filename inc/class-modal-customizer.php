76b9fb829f88bf7ba1b91bab9f29ad79fd2f8662
Steven K Word <stevenword@gmail.com>
2014-03-15 17:12:31 -0400

    Debugging

--- inc/class-modal-customizer.php
+++ inc/class-modal-customizer.php
@@ -31,7 +31,7 @@ class WP_Present_Modal_Customizer {
 		if( ! is_admin() || ! isset( $_REQUEST['tag_ID'] ) )
 			return;
 
-		//add_action( 'plugins_loaded', array( $this, 'action_plugins_loaded' ) );
+		add_action( 'plugins_loaded', array( $this, 'action_plugins_loaded' ) );
 
 		// Let's roll.
 		add_action( 'wp_loaded', array( $this, 'action_wp_loaded' ) );
@@ -86,7 +86,7 @@ class WP_Present_Modal_Customizer {
 
 	public function action_plugins_loaded() {
 
-		require( ABSPATH . WPINC . '/class-wp-customize-manager.php' );
+		//require( ABSPATH . WPINC . '/class-wp-customize-manager.php' );
 
 		// Init Customize class
 		$GLOBALS['wp_customize'] = new WP_Customize_Manager;
