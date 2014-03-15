97dbed187d6cb33144a1edb962f44fbac532d037
Steven K Word <stevenword@gmail.com>
2014-03-15 17:11:57 -0400

    Debugging

--- inc/class-modal-customizer.php
+++ inc/class-modal-customizer.php
@@ -31,9 +31,7 @@ class WP_Present_Modal_Customizer {
 		if( ! is_admin() || ! isset( $_REQUEST['tag_ID'] ) )
 			return;
 
-return;
-
-		add_action( 'plugins_loaded', array( $this, 'action_plugins_loaded' ) );
+		//add_action( 'plugins_loaded', array( $this, 'action_plugins_loaded' ) );
 
 		// Let's roll.
 		add_action( 'wp_loaded', array( $this, 'action_wp_loaded' ) );
