ee2e5ad63fa3e926fcf6eba076faab633a3d5fee
Steven K Word <stevenword@gmail.com>
2014-03-15 16:30:18 -0400

    Capability management

--- inc/class-wp-present-core.php
+++ inc/class-wp-present-core.php
@@ -226,7 +226,7 @@ class WP_Present_Core {
 			),
 			'hierarchical'      => true,
 			'show_ui'           => true,
-			'show_admin_column' => true,
+			'show_admin_column' => false,
 			'query_var'         => true,
 			'rewrite'           => array(
 				'slug'   => self::TAXONOMY_SLUG,
